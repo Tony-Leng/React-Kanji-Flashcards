29970f98ed16f2e159869f2612582035f961c6fb
Tony Leng <lengtk@gmail.com>
2023-06-30 17:50:53 +0900

    inital commit, created NavBar and Card components

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+
+
+export default function Card() {
+  return (
+    <div>
+      <h1>CARD GOES HERE</h1>
+      <button>Next</button>
+    </div>
+  );
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+
+
+export default function NavBar() {
+  return (
+    <div>
+      <h1>Kanji Flashcards</h1>
+    </div>
+  );
+}
