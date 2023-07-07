ff0e9847642e1ff95fdae48fdc6a1b7c8fa0c1c6
Tony Leng <lengtk@gmail.com>
2023-07-07 18:14:55 +0900

    added one more kanji to kanji list

--- src/App.js
+++ src/App.js
@@ -5,24 +5,32 @@ import Card from './components/Card';
 
 
 function App() {
-  const [currentKanji, setCurrentKanji] = useState([
-    {
-      id: 1,
-      character: "日",
-      onyomi: "nichi, jitsu",
-      kunyomi: "hi, -bi, -ka",
-      meaning: "Day, sun, Japan, counter for days",
-      jlpt: "N5",
-    },
-    {
-      id: 2,
-      character: "一",
-      onyomi: "ichi",
-      kunyomi: "hito(tsu)",
-      meaning: "One",
-      jlpt: "N5",
-    },
-  ]);
+  const [currentKanji, setCurrentKanji] = useState(
+  {
+    id: 1,
+    character: "日",
+    onyomi: "nichi, jitsu",
+    kunyomi: "hi, -bi, -ka",
+    meaning: "Day, sun, Japan, counter for days",
+    jlpt: "N5",
+  },
+  {
+    id: 2,
+    character: "一",
+    onyomi: "ichi",
+    kunyomi: "hito(tsu)",
+    meaning: "One",
+    jlpt: "N5",
+  },
+  {
+    id: 3,
+    character: "国",
+    onyomi: "koku",
+    kunyomi: "kuni",
+    meaning: "Country",
+    jlpt: "N5",
+  });
+
 
   return (
     <div className="App">
