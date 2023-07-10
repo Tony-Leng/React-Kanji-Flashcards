81b53f3c571e657614b2c0a5eca68ca93d14172e
Tony Leng <lengtk@gmail.com>
2023-07-10 21:16:29 +0900

    created useEffect with random number generator to select kanji from kanjiList

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import NavBar from './components/NavBar';
 import Card from './components/Card';
 
 
-function App() {
-  const [currentKanji, setCurrentKanji] = useState(
-  {
-    id: 1,
-    character: "日",
-    onyomi: "nichi, jitsu",
-    kunyomi: "hi, -bi, -ka",
-    meaning: "Day, sun, Japan, counter for days",
-    jlpt: "N5",
-  },
-  {
-    id: 2,
-    character: "一",
-    onyomi: "ichi",
-    kunyomi: "hito(tsu)",
-    meaning: "One",
-    jlpt: "N5",
-  },
-  {
-    id: 3,
-    character: "国",
-    onyomi: "koku",
-    kunyomi: "kuni",
-    meaning: "Country",
-    jlpt: "N5",
-  });
 
+function App() {
+  const kanjiList = [
+    {
+      id: 1,
+      character: "日",
+      onyomi: "nichi, jitsu",
+      kunyomi: "hi, -bi, -ka",
+      meaning: "Day, sun, Japan, counter for days",
+      jlpt: "N5",
+    },
+    {
+      id: 2,
+      character: "一",
+      onyomi: "ichi",
+      kunyomi: "hito(tsu)",
+      meaning: "One",
+      jlpt: "N5",
+    },
+    {
+      id: 3,
+      character: "国",
+      onyomi: "koku",
+      kunyomi: "kuni",
+      meaning: "Country",
+      jlpt: "N5",
+    }
+  ]
+
+  const [currentKanji, setCurrentKanji] = useState();
+
+  useEffect(() => {
+    const kanji = kanji[Math.floor(Math.random() * kanji.length)];
+    return kanji;
+  }, []); //dependency array
 
   return (
     <div className="App">
       <NavBar />
-      <Card currentKanji={currentKanji}/>
+      <Card kanjiList={kanjiList}/>
     </div>
   );
 }
