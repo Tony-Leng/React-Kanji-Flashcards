31684e30019486e1975341a53aa8f4a5ca7e731c
Tony Leng <lengtk@gmail.com>
2023-07-10 21:22:22 +0900

    cleaned up Card.js, created variable from props to render in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,14 @@
 import React from "react";
 
 const Card = (props) => {
-  const kanjiList = props.currentKanji;
+  const displayKanji = props.currentKanji;
 
   return (
     <div>
-      {kanjiList.map((kanji) => (
-        <div>{ kanji.character }</div>
-      ))}
+        <div>{ displayKanji }</div>
       <button>Next Kanji</button>
     </div>
   )
 }
 
 export default Card;
-
-
-  // return (
-  //   // <div className="card-body">
-  //   //   <section className="card-front">
-  //   //     <h1>Kanji</h1>
-  //   //     <button>Next</button>
-  //   //   </section>
-  //   // </div>
-  // );
