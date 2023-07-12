55f8394fe156b86f78a9e974bd817c9d9c4726f9
Tony Leng <lengtk@gmail.com>
2023-07-12 22:27:52 +0900

    reverted back to previous version

--- src/components/button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Button(props) {
-  const getKanji = [props.getKanji]
-
-  return (
-    <button onClick={ getKanji }>Next Kanji</button>
-  )
-}
