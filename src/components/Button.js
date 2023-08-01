0c95d42eed4d637b7c982f36ea723b19e71ac2df
Tony Leng <lengtk@gmail.com>
2023-08-01 23:11:38 +0900

    added coming soon alert to Filter Kanji button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default function Button(props) {
   const getKanji = props.getKanji;
+  const filterKanji = () => {
+    alert("Coming Soon :)")
+  };
 
   return (
     <div className='buttons'>
       <button onClick={getKanji} className='buttons-nextkanji'>
         <h3>Next Kanji</h3>
       </button>
-      <button className='buttons-filterkanji'>
+      <button onClick={filterKanji} className='buttons-filterkanji'>
         <h3>Filter Kanji</h3>
       </button>
     </div>
